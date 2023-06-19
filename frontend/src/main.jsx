3be0139c3a2ba95664a92198ff5ae1a4dc753fde
Samir-zd <samir.zahreddine@outlook.com>
2023-06-20 02:31:26 +0300

    Added Redux-Toolkit for State-Management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {createBrowserRouter, createRoutesFromElements, Route, Router, RouterProvider} from 'react-router-dom'
+import store from './store.js'
+import { Provider } from 'react-redux'
+import { createBrowserRouter, createRoutesFromElements, Route, Router, RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
-
 import LoginPage from './pages/LoginPage.jsx'
 import RegisterPage from './pages/RegisterPage.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/'element={<App/>}>
-    <Route  path='/login'element={<LoginPage/>}/>
-    <Route  path='/register'element={<RegisterPage/>}/>
+    <Route path='/' element={<App />}>
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
     </Route>
   )
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
+ <Provider store={store}>
+
+ <React.StrictMode>
+    <RouterProvider router={router} />
   </React.StrictMode>,
+ </Provider>
 )
